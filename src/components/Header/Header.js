e85f7d8a6627eae584ee8a8e3f144a8893bb48be
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-05-01 10:39:47 +0530

    Added header styles and logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import "./Header.css"
+import "../../assets"
+import { logo } from '../../assets'
 
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#"><img src={logo} alt="Logo"/> </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link active" aria-current="page" href="#">SERVICES</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">ABOUT US</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link" href="#">CONTACT US</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a className="nav-link">CAREERS</a>
             </li>
           </ul>
         </div>
